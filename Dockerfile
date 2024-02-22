6160cb13dbbfa0f546fc855a8b448a8f5beb1791
Louie Celli <v-lcelli@youngliving.com>
2024-02-22 15:46:20 -0700

    Moving functions to a single file.

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN unzip awscliv2.zip
 RUN ./aws/install
 COPY --from=truemark/jq:latest /usr/local/ /usr/local/
 COPY helper.sh /usr/local/bin/helper.sh
-COPY run.sh /usr/local/bin/run.sh
-RUN chmod +x /usr/local/bin/run.sh
+#COPY run.sh /usr/local/bin/run.sh
+RUN chmod +x /usr/local/bin/helper.sh
 
-ENTRYPOINT ["/usr/local/bin/run.sh"]
+ENTRYPOINT ["/usr/local/bin/helper.sh"]
--- helper.sh
+++ helper.sh
@@ -241,3 +241,33 @@ function initialize() {
   if_aws_account_id
   if_local_path
 }
+
+function aws_secrets_manager_parser() {
+  echo "Pulling the secret with name ${AWS_SECRET_NAME} from AWS Secrets Manager."
+
+  if [ -z "${AWS_SECRET_NAME}" ]; then
+    echo -e "Error: AWS_SECRET_NAME environment variable is not set.\n Using the credentials that are being passed in."
+  fi
+
+  export FLYWAY_URL=$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRET_NAME} | jq -r '.SecretString' | jq -r '.host')
+  if [ ! -z "${FLYWAY_URL}" ]; then
+    echo -e "\nYour FLYWAY_URL has been set."
+  fi
+
+  export FLYWAY_USER=$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRET_NAME} | jq -r '.SecretString' | jq -r '.username')
+  if [ ! -z "${FLYWAY_USER}" ]; then
+      echo "Your FLYWAY_USER has been set."
+  fi
+
+  export FLYWAY_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRET_NAME} | jq -r '.SecretString' | jq -r '.password')
+  if [ ! -z "${FLYWAY_PASSWORD}" ]; then
+      echo -e "Your FLYWAY_PASSWORD has been set.\n"
+  fi
+}
+
+initialize
+aws_secrets_manager_parser
+# Execute command(s)
+for CMD in "${!COMMAND@}"; do
+  eval "${!CMD}"
+done
