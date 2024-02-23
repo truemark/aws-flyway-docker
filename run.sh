f7606e4a63ef09bba6cf3e1692883e540332584a
Louie Celli <v-lcelli@youngliving.com>
2024-02-23 09:28:30 -0700

    Adding in a file path variable.

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+set -xe
 # Exit on command failure and unset variables
 set -euo pipefail
 
@@ -11,6 +12,11 @@ function aws_secrets_manager_parser() {
 
   if [ -z "${AWS_SECRET_NAME}" ]; then
     echo -e "Error: AWS_SECRET_NAME environment variable is not set.\n Using the credentials that are being passed in."
+  fi
+
+  if [ -z "${CONF_FILE_PATH}" ]; then
+    echo -e "Error: CONF_FILE_PATH environment variable is not set."
+    exit 1
   fi
 
   export FLYWAY_URL=$(aws secretsmanager get-secret-value --secret-id ${AWS_SECRET_NAME} | jq -r '.SecretString' | jq -r '.host')
@@ -31,6 +37,11 @@ function aws_secrets_manager_parser() {
 
 initialize
 aws_secrets_manager_parser
+
+echo "flyway.url=$FLYWAY_URL" >> ${CONF_FILE_PATH}
+echo "flyway.user=$FLYWAY_USER" >> ${CONF_FILE_PATH}
+echo "flyway.password=$FLYWAY_PASSWORD" >> ${CONF_FILE_PATH}
+
 # Execute command(s)
 for CMD in "${!COMMAND@}"; do
   eval "${!CMD}"
